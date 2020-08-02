10a4041e4c814016ac60d3fdbb45dae80560c365
wayneluong <wayneluong93@gmail.com>
2020-08-02 20:31:24 +0100

    vue scss

---
+++ vue-scss/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>
+      Test
+      <span>Message</span>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='scss' scoped>
+@import "../assets/scss/main";
+
+.hello {
+  h1 {
+    color: red;
+  }
+}
+</style>
